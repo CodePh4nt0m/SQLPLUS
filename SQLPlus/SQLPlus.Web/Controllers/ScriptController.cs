186653cc5910ec193e4ae88379831d41603d9a1f
CodePh4nt0m <dilnuwan77@gmail.com>
2016-07-08 01:07:29 +0530

    styles

--- SQLPlus/SQLPlus.Web/Controllers/ScriptController.cs
+++ SQLPlus/SQLPlus.Web/Controllers/ScriptController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SQLPlus.Web.Models.ViewModels;
 
 namespace SQLPlus.Web.Controllers
 {
@@ -16,7 +17,19 @@ namespace SQLPlus.Web.Controllers
 
         public ActionResult Viewer()
         {
-            return View();
+            var model = new ScriptViewerViewModel();
+            model.Query = @"CREATE TABLE ""topic"" (
+    ""id"" serial NOT NULL PRIMARY KEY,
+    ""forum_id"" integer NOT NULL,
+    ""subject"" varchar(255) NOT NULL
+);
+ALTER TABLE ""topic""
+ADD CONSTRAINT forum_id FOREIGN KEY (""forum_id"")
+REFERENCES ""forum"" (""id"");
+-- Initials
+insert into ""topic"" (""forum_id"", ""subject"")
+values (2, 'D''artagnian');";
+            return View(model);
         }
     }
 }
---
+++ SQLPlus/SQLPlus.Web/Models/ViewModels/ScriptViewerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SQLPlus.Web.Models.ViewModels
+{
+    public class ScriptViewerViewModel
+    {
+        public string SpName { get; set; }
+        public string Query { get; set; }
+    }
+}
