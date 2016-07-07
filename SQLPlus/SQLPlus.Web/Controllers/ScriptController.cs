e1cc8d3a28ba671e9fdaf1974839404f20d2bb6a
CodePh4nt0m <dilnuwan77@gmail.com>
2016-07-07 15:17:23 +0530

    viewer

---
+++ SQLPlus/SQLPlus.Web/Controllers/ScriptController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQLPlus.Web.Controllers
+{
+    public class ScriptController : Controller
+    {
+        // GET: Script
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Viewer()
+        {
+            return View();
+        }
+    }
+}
