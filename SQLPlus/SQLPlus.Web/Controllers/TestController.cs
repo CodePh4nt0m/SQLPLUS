2a07059b46b13431fe24d71d7413e597c2a8ed2e
CodePh4nt0m <dilnuwan77@gmail.com>
2016-07-08 18:39:40 +0530

    spinner

---
+++ SQLPlus/SQLPlus.Web/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQLPlus.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Spinner()
+        {
+            return View();
+        }
+
+        public ActionResult Spinner2()
+        {
+            return View();
+        }
+    }
+}
